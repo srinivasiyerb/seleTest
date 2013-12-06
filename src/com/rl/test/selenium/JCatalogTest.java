37cce36d448a9fa0b3b9f66959bc92d25d53be0f
srini <srinivasiyerb@gmail.com>
2013-12-06 17:10:52 +0530

    Test

---
+++ src/com/rl/test/selenium/JCatalogTest.java
@@ -0,0 +1,97 @@
+package com.rl.test.selenium;
+
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import org.testng.annotations.Test;
+
+public class JCatalogTest extends MainDriver{
+	
+	public static void openBrowser(){
+		File file = new File("src/IEDriverServer.exe");
+		System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
+		driver = new FirefoxDriver();
+		driver.get("http://192.168.103.37:8080/catalog/");		
+	}
+	public static void closeBrowser(){
+		driver.close();
+	}
+	public static void login(){
+		
+		driver.findElement(By.linkText("login")).click();
+		driver.findElement(By.xpath("//input[@id='loginForm:username']")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@id='loginForm:password']")).sendKeys("admin");
+		//driver.findElement(By.xpath("//input[@name='loginForm:_id24")).click();
+		driver.findElement(By.name("loginForm:_id24")).click();
+		}
+	 
+	@Test
+	public void TestcategoryOne(){
+		try{
+		openBrowser();
+		login();
+		Category.categoryOne();
+		}
+		catch(Exception e){
+			System.out.print(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void Testcategorytwo(){
+		
+		Category.categoryTwo();
+		}
+	
+	@Test
+	public void Testcategorythree(){
+		
+		Category.categoryThree();
+		
+	}
+	
+	@Test
+	public void Testcategoryfour(){
+		
+		Category.categoryFour();
+		
+		}
+	
+	@Test
+	public void TestproductList(){
+		
+		NewProduct.ProductList();
+		
+	}
+
+	@Test
+	public void TestnewProduct(){
+		try{
+		
+		
+		NewProduct.newproduct();
+		
+		}
+		catch(Exception e){
+			System.out.print(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void Testcontactus(){
+		
+		ContactUs.contactUs();
+		
+	}
+	
+	@Test
+	public void Testplaceorder(){
+		
+		PlaceOrder.placeOrder();
+		
+	}
+
+}

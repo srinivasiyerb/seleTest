78c839ddc031172ca420da396b6bd94f4fbb9a43
srinivas <srinivasiyerb@gmail.com>
2013-12-18 10:05:02 +0530

    Update MainDriver.java

--- src/com/rl/test/selenium/MainDriver.java
+++ src/com/rl/test/selenium/MainDriver.java
@@ -1,7 +1,9 @@
 package com.rl.test.selenium;
 
-import org.openqa.selenium.WebDriver;
-
+//import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 public class MainDriver {
-	static WebDriver driver;
+	//static WebDriver driver;
+	static HtmlUnitDriver driver;
+
 }
